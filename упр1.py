c105cdcb32fe5744ddaa9e20f933bac8411fe6ae
Radosll <36232344+Radosll@users.noreply.github.com>
2018-02-21 12:44:18 +0200

    Add files via upload

---
+++ упр1.py
@@ -0,0 +1,16 @@
+
+age = float(input('Enter your age:'))
+while age>7
+if age=<7:
+    print('stava')
+else:
+    print ('too old')
+
+    print ('probvai pak')
+do
+age = float(input('Enter your age:'))
+
+if age < 7:
+    print('stava')
+else:
+    print('too old')
